0d8ccbfa68a0f22b54af2a724b313723aa4c098d
Tyler Mayoff <tyler@tylermayoff.com>
2020-12-18 13:57:41 -0500

    Need to add registering and loging in

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,56 @@
 import auth from './auth';
 import { Router } from 'express';
+import passport from 'passport';
 var router = Router();
 
 /* GET home page. */
 router.get('/', auth.optional, (req, res, next) => {
+  const { body: { user } } = req;
+  if (user) {
+    res.render("index");
+  } else {
+    res.redirect("login");
+  }
+});
 
-  res.render('index', { title: 'Express' });
+router.get('/login', auth.optional, (req, res, next) => {
+
+  res.render("login");
+});
+
+router.post('/login', auth.optional, (req, res, next) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  // Errors
+  let errors = {};
+  if (!email)
+    errors.email = "Email is required";
+  if (!password)
+    errors.password = "Password is required";
+  if (!email || !password)
+    return res.render("login", errors);
+
+  // Authenticate user
+  passport.authenticate('local', { session: false }, (err, passportUser, info) => {
+    if (err)
+      return next(err);
+
+    if (passportUser) {
+      const user = passportUser;
+      user.token = passportUser.generateJWT();
+
+      return res.json({ user: user.toAuthJSON() })
+    }
+
+    return status(400).info;
+  })(req, res, next);
+
+  res.render("login", errors);
+})
+
+router.get('/register', auth.optional, (req, res, next) => {
+  res.render("login");
 });
 
 export default router;
--- services/passport.js
+++ services/passport.js
@@ -1,9 +1,9 @@
-import { use } from 'passport';
+import passport from 'passport';
 import LocalStrategy from 'passport-local';
 
-use(new LocalStrategy({
-    username: 'user[email]',
-    password: 'user[password]'
+passport.use(new LocalStrategy({
+    usernameField: 'user[email]',
+    passwordField: 'user[password]',
 }, (email, password, done) => {
     // TODO Find and authenticate user
     return done(null, false, { errors: { 'Not implemented': 'Authentication not implemented' } });
