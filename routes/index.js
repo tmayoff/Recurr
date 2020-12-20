a2ce2aff7bcbe69478c1533992dcd399209fe2a0
tyler <tyler@tylermayoff.com>
2020-12-20 13:40:39 -0500

    Fixed modals added more buttons

--- routes/index.js
+++ routes/index.js
@@ -7,13 +7,36 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', auth.isAuthenticated, (req, res, next) => {
+  let promises = [];
 
-  RecurPay.findAll({
+  promises.push(RecurPay.findAll({
     where: {
       userId: req.user.id
     }
-  }).then(list => {
-    res.render('index', { user: req.user, list });
+  }));
+
+  promises.push(RecurPay.findAll({
+    attributes: ['price', 'cycletype']
+  }));
+
+
+  Promise.all(promises).then((results) => {
+    // Calculate summary
+    let monthly = 0;
+    let yearly = 0;
+    results[1].forEach(obj => {
+      switch (obj.cycletype) {
+        case "Monthly":
+          monthly += obj.price;
+          break;
+        case "Yearly":
+          yearly += obj.price;
+          break;
+      }
+    });
+
+    monthly += yearly / 12;
+    res.render('index', { user: req.user, list: results[0], monthly });
   });
 });
 
